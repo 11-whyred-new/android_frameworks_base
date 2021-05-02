403c1c78f84817b398c29e949f730097c32aa1ca
Mohan C M <mohanmanjappa@gmail.com>
2021-05-02 06:50:44 +0000

    Revert "base: Global Actions with Advanced Reboot [1/2]"
    This reverts commit 2f687410d02b6681e173fe0baeb28d0872fdb2b8.
    
    Change-Id: I8bad93e6f84e1f2169426d0446c88742309f4596

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4943,11 +4943,6 @@ public final class Settings {
         public static final String CAMERA_LAUNCH = "camera_launch";
 
         /**
-         * @hide
-         */
-        public static final String GLOBAL_ACTIONS_LIST = "global_actions_list";
-
-        /**
          * Whether allowing pocket service to register sensors and dispatch informations.
          *   0 = disabled
          *   1 = enabled
@@ -9242,11 +9237,6 @@ public final class Settings {
          * @hide
          */
         public static final String NOTIFICATION_DISMISS_RTL = "notification_dismiss_rtl";
-
-        /**
-         * @hide
-         */
-        public static final String ADVANCED_REBOOT = "advanced_reboot";
 
         /**
          * Comma separated list of QS tiles that have been auto-added already.
